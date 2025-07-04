399a66f5154d848c23de1022b04b9035b2ca4541
acuavista <stanp193@gmail.com>
2025-07-04 11:35:40 -0400

    Added the comas back after the image file names

--- src/data/lotCategories.ts
+++ src/data/lotCategories.ts
@@ -3,7 +3,7 @@ export const lotCategories = [
   {
     title: "Beachfront Properties",
     description: "Wake up to the Caribbean Sea. Direct access to pristine sands and unparalleled views. Your ultimate luxury escape.",
-    image: import.meta.env.BASE_URL + "lovable-uploads/a9b17377-c565-45d2-98c3-326004ae6ce8.png"
+    image: import.meta.env.BASE_URL + "lovable-uploads/a9b17377-c565-45d2-98c3-326004ae6ce8.png",
       
     price: "$200,000",
     pricePerSqFt: "$100/sf"
@@ -19,7 +19,7 @@ export const lotCategories = [
   {
     title: "Tropical Paradise Properties",
     description: "Secluded havens nestled amidst lush Colombian landscapes. Ideal for tranquil retreats and connecting with nature.",
-    image: import.meta.env.BASE_URL + "lovable-uploads/46049fac-37d0-4492-b0c8-8141c080f816.png"
+    image: import.meta.env.BASE_URL + "lovable-uploads/46049fac-37d0-4492-b0c8-8141c080f816.png",
     price: "$55,000",
     pricePerSqFt: "$27.5/sf"
   }
