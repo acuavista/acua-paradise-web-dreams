4cd645dd7fcc1231b71e4f224d6b0778a5441e56
acuavista <stanp193@gmail.com>
2025-07-08 07:37:02 -0400

    Update ContactSection.tsx removed redundant line from the top of the page
    import React from 'react';

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,7 +1,7 @@
 // This is a dummy change to force a new Netlify deploy and re-detection.
 import React from 'react';
 // ... rest of your code ...
-import React from 'react';
+
 
 const ContactSection: React.FC = () => {
   return (
