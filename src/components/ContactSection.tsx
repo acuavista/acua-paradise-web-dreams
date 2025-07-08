57afc32742d0c4b56a0ea6161c7f21ae868f38a8
acuavista <stanp193@gmail.com>
2025-07-08 05:56:56 -0400

    Update ContactSection.tsx added needed line for netlify's captcha
    <form name="contact" data-netlify="true" data-netlify-recaptcha="true" className="space-y-6">

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -14,7 +14,7 @@ const ContactSection: React.FC = () => {
         </div>
 
         <div className="glass-card p-8 animate-on-scroll opacity-0 translate-y-8 transition-all duration-1000">
-          <form name="contact" data-netlify="true" className="space-y-6">
+          <form name="contact" data-netlify="true" data-netlify-recaptcha="true" className="space-y-6"> 
             {/* NEW FORM FIELDS START HERE */}
             <div className="grid md:grid-cols-2 gap-6">
               <div>
