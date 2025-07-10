c7018a4970cc14c775c1836785c753bd26a8a9ee
acuavista <stanp193@gmail.com>
2025-07-09 20:34:20 -0400

    Update WhyCards.ts  changed image path for the last 3 card updates
    "/public/lovable-uploads/cruiseShips.png"

--- src/data/WhyCards.ts
+++ src/data/WhyCards.ts
@@ -1,19 +1,19 @@
 export const whyCardsData = [
   {
     title: "Why Cartagena?",
-    image: "/public/images/beachNbuildings.png",
+    image: "/public/lovable-uploads/beachNbuildings.png",
     excerpt: "There are places that capture your attention—and then there are places like Cartagena, that steal your heart entirely. Draped in the vibrant hues of its colonial past and pulsing with the rhythm of modern life, Cartagena is where charm, culture, comfort, and opportunity converge in one irresistible package.",
     link: "/why-cartagena"
   },
   {
     title: "Why Real Estate?",
-    image: "/public/images/saturated.png",
+    image: "/public/lovable-uploads/saturated.png",
     excerpt: "Over the past five years, property values in Cartagena have appreciated between 8% and 12% annually, depending on the neighborhood and type of investment. This is no accident. The city has seen a surge in both domestic and foreign demand, with buyers ranging from vacation homeowners to entrepreneurs and digital nomads drawn to the warm climate and vibrant urban life.",
     link: "/why-real-estate"
   },
   {
     title: "Why Now?",
-    image: "/public/images/cruiseShips.png",
+    image: "/public/lovable-uploads/cruiseShips.png",
     excerpt: "With over 3.5 million visitors in 2024, Cartagena’s international profile is growing rapidly. And with the completion of the 2nd international airport, the city is expecting to quadruple the number of visitors. As demand increases, so do prices. Those who act now are getting in on the ground floor of a market destined for exponential growth.",
     link: "/why-now"
   }
